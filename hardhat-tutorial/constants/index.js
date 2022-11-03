ab68db92443afd5e8f0d5d8fecfb374e0f37669e
JayGalvan90 <39308108+JayGalvan90@users.noreply.github.com>
2022-11-03 19:59:51 -0400

    NFT Collection connected to whitelist
    First NFT collection being pushed and connected to whitelist Project.

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x7d93E721F1301e50eEafE727690c413E399Ecbb5";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
